4a65d7359683b1e697261b561c0e4072a4592935
shailesh <shaileshkryadav13@gmail.com>
2026-07-12 10:45:39 -0500

    project initlized

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import {createServer} from "node:http";
+
+import {Server} from "socket.io";
+import { connectToSocket } from "./src/controllers/socketManager.js";
+
+import mongoose from "mongoose";
+
+import cors from "cors";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb", extended: true}));
+
+app.get("/", (req,res)=>{
+    res.send("Hello world");
+})
+const start = async ()=>{
+    const connectionDB = await mongoose.connect("mongodb+srv://weChat:GQW2xpJBUU7CKUYV@wechat.jmunmgu.mongodb.net/?appName=WeChat")
+    console.log(`Mongo connected DB host : ${connectionDB.connection.host}`);
+server.listen(app.get("port"), ()=> {
+    console.log("The server has started");
+})
+
+}
+
+
+start();
